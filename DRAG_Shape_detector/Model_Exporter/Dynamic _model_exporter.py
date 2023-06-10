bcf7923c078a4faacbd9561f6387d6057dabcc37
osamaalsahafi <53305359+osamaalsahafi@users.noreply.github.com>
2023-06-11 00:29:30 +0300

    Configure paths and folders

--- DRAG_Shape_detector/Model_Exporter/Dynamic _model_exporter.py
+++ DRAG_Shape_detector/Model_Exporter/Dynamic _model_exporter.py
@@ -1,5 +1,5 @@
 from ultralytics import YOLO
 
-path = "runs/detect/train/weights/best_dynamic.onnx"
+path = "../runs/detect/train/weights/best_dynamic.onnx"
 model = YOLO(path)
 model.export(format="onnx", dynamic=True)
