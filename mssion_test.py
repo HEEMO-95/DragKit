58e9863104d3ccd803007eea8b7463a91643449b
HEEMO-95 <81169269+HEEMO-95@users.noreply.github.com>
2023-06-14 19:39:21 +0300

    1

--- mssion_test.py
+++ mssion_test.py
@@ -2,16 +2,17 @@ from DragKit import *
 import socket
 
 HOST = 'localhost'
-PORT = 1995
+PORT1 = 1995
+PORT2 = 1416
 mysocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-mysocket.connect((HOST, PORT))
+mysocket.connect((HOST, PORT1))
 
 def mission():
     action = 'mission'
     
     '''if recived a new opject detection message from server'''
     message = mysocket.recv(1024).decode('utf-8')  # function will halt here until data arrvie
-    if message = 'mission_done':
+    if message == 'mission_done':
         flight_mode('RTL')
 
     if message == 'detected':  # if data arrived, check it and proceed
@@ -19,19 +20,12 @@ def mission():
 
     if action == 'stopped':
         ''' pass postion port to the align function which has a client socket to the ID_server'''
-        action = align(PORT)
+        action = align(PORT2)
 
     if action == 'aligned':
         '''get shape data from shape server'''
-        mysocket.connect((HOST, PORT))  # connect to opject identification server
-
-
-    message = mysocket.recv(1024).decode('utf-8')
-    winched = winch(message)
-
-    mysocket.send('align'.encode('utf-8'))
-    if winched == True: 
-        resume()
+        message = mysocket.recv(1024).decode('utf-8')
+        print(message)
 
 while True:
     mission() 
